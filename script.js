f3bc8c98597787e8d7ed0fcf69166853e0cc86ed
Alexis S Liu <syl1350@gmail.com>
2021-08-02 20:49:04 -0400

    added image to movie container

--- script.js
+++ script.js
@@ -14,22 +14,37 @@ function selectGenre(e){
   const genreOpt = e.target.querySelector("#genres")
   const genre = genreOpt.options[genreOpt.selectedIndex].value
   fetchMovie(genre)
+  console.log(genre)
 }
 
 function fetchMovie(genre){
-  const pageNo = Math.floor(Math.random() * 101)
+  const pageNo = Math.floor(Math.random() * 501)
   let requestURL = `${URL}${genre}&page=${pageNo}`
   fetch(requestURL).then(resp => resp.json()).then(renderMovie)
+  // console.log(requestURL)
 }
 
 function renderMovie(movie){
-
+console.log(movie)
   // console.log(movie.results[0].original_title)
+
   const movieRec = document.createElement("div");
   movieRec.className = 'rec'
-  movieRec.innerText = movie.results[0].original_title
+  const random = Math.floor(Math.random() * 21)
+  movieRec.innerText = movie.results[random].original_title
+  
+  const moviePoster = document.createElement("img");
+  const basePosterURL = 'https://image.tmdb.org/t/p/w500'
+  moviePoster.src = basePosterURL+movie.results[random].poster_path
+  moviePoster.className = "movie-poster";
+  
+  // console.log (random)
+  console.log(moviePoster.src)
   const movieContainer = document.querySelector(".movie-container")
-  movieContainer.append(movieRec)
+  movieContainer.innerHTML = ''
+  movieContainer.append(movieRec, moviePoster)
+
+
 }
 
 
