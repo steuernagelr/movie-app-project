707bc04203854e29425b1382feb2e136de8acc9b
Alexis S Liu <syl1350@gmail.com>
2021-08-05 11:23:00 -0400

    added review form popup

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 //api key api_key=30170f3751cc29da3d08369d25340c51
 const URL =
   // "https://api.themoviedb.org/3/discover/movie?api_key=30170f3751cc29da3d08369d25340c51&language=en-US&region=US&with_genres=";
-  "https://api.themoviedb.org/3/discover/movie?api_key=30170f3751cc29da3d08369d25340c51&language=en-US&with_original_language=en&region=US&include_adult=false&include_video=false&with_genres="
+  "https://api.themoviedb.org/3/discover/movie?api_key=30170f3751cc29da3d08369d25340c51&language=en-US&with_original_language=en&region=US&include_adult=false&include_video=false&with_genres=";
 
 const mForm = document.querySelector(".movieForm");
 console.log(mForm);
 mForm.addEventListener("submit", fetchGenre);
 
-fetchMyMovieList()
+fetchMyMovieList();
 
 function fetchGenre(e) {
   e.preventDefault();
@@ -40,7 +40,7 @@ function fetchMovie(genre) {
   // console.log(genre);
   // console.log(pageNo);
   let requestURL = `${URL}${genre}&page=${pageNo}`;
-  console.log(requestURL)
+  console.log(requestURL);
   fetch(requestURL)
     .then((resp) => resp.json())
     .then(randomize);
@@ -49,18 +49,18 @@ function fetchMovie(genre) {
 }
 
 function randomize(movies) {
-  console.log(movies)
+  console.log(movies);
   const movieIndex = Math.floor(Math.random() * 20);
-  console.log(movieIndex)
-  const movie = movies.results[movieIndex]
+  console.log(movieIndex);
+  const movie = movies.results[movieIndex];
   // console.log(movie.results)
-  renderMovie(movie)
+  renderMovie(movie);
 }
 
 function renderMovie(movie) {
   // console.log(movie);
   // console.log(movie.results[0].original_title)
-  console.log(movie)
+  console.log(movie);
   const movieCard = document.createElement("div");
   movieCard.className = "movie-card";
   if (movieCard.classList.contains("fade-in")) {
@@ -77,8 +77,8 @@ function renderMovie(movie) {
   const moviePoster = document.createElement("img");
   const basePosterURL = "https://image.tmdb.org/t/p/w400";
 
-  const movieDesc = document.createElement("p")
-  movieDesc.textContent = movie.overview
+  const movieDesc = document.createElement("p");
+  movieDesc.textContent = movie.overview;
 
   moviePoster.src = basePosterURL + movie.poster_path;
   // const imgURL = moviePoster.src;
@@ -95,7 +95,6 @@ function renderMovie(movie) {
 }
 
 function renderMyMovieList(movie) {
-  
   const movieCard = document.createElement("div");
   movieCard.className = "movie-card";
   if (movieCard.classList.contains("fade-in")) {
@@ -117,82 +116,105 @@ function renderMyMovieList(movie) {
   moviePoster.className = "movie-poster";
 
   const movieList = document.querySelector(".movieList");
-  const movieListH2 = document.createElement("h2")
-  movieListH2.textContent = "My Movies"
+  const movieListH2 = document.createElement("h2");
+  movieListH2.textContent = "My Movies";
 
-  const reviewButton = document.createElement("button")
-  reviewButton.textContent = "Add a review"
+  const reviewButton = document.createElement("button");
+  reviewButton.textContent = "Add a review";
+  reviewButton.classList.add("openBttn");
+  reviewButton.id = "openBttn";
 
-  const deleteButton = document.createElement("button")
-  deleteButton.textContent = "Delete"
-  deleteButton.id = 'delete_movie'
+  reviewButton.addEventListener("click", openTheForm);
 
-  deleteButton.addEventListener('click', () => {
-      movieCardContainer.remove()
-      deleteMovie(movie.id)
-})
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.id = "delete_movie";
 
-function deleteMovie(id){
-    fetch(`http://localhost:3000/movieList/${id}`,{
-        method:'DELETE',
-        headers: {
-            'Content-Type':'application/json'
-        }
+  deleteButton.addEventListener("click", () => {
+    movieCardContainer.remove();
+    deleteMovie(movie.id);
+  });
+
+  function deleteMovie(id) {
+    fetch(`http://localhost:3000/movieList/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-        .then(res => res.json())
-        .then(movie => console.log(movie))
-   }
+      .then((res) => res.json())
+      .then((movie) => console.log(movie));
+  }
 
-
-  const movieCardContainer = document.createElement("div")
-  movieCardContainer.classList.add("card-container")
-  movieCardContainer.append(movieCard, reviewButton, deleteButton)
+  const movieCardContainer = document.createElement("div");
+  movieCardContainer.classList.add("card-container");
+  movieCardContainer.append(movieCard, reviewButton, deleteButton);
   movieCard.append(movieTitle, moviePoster);
   movieList.prepend(movieCardContainer);
   // const movieListh2 = document.createElement("h2")
   // movieListh2.textContent = "My Movies"
   // if (movieListh2.textcontent = "")
   // movieList.prepend(movieListh2)
-  mcontainer = document.querySelector(".movie-container")
-  mcontainer.innerHTML = ""
-  console.log(mcontainer)
+  mcontainer = document.querySelector(".movie-container");
+  mcontainer.innerHTML = "";
+  console.log(mcontainer);
 }
 
 function saveMovie(e, movie) {
+  e.preventDefault();
+  console.log(movie);
 
-  e.preventDefault()
-  console.log(movie)
-
-    renderMyMovieList(movie)
-    fetch("http://localhost:3000/movieList", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(movie),
-    })
-  
+  renderMyMovieList(movie);
+  fetch("http://localhost:3000/movieList", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(movie),
+  });
 }
 
 function fetchMyMovieList() {
- 
   fetch("http://localhost:3000/movieList")
     .then((resp) => resp.json())
-    .then(o => o.forEach(renderMyMovieList));
-
+    .then((o) => o.forEach(renderMyMovieList));
 }
 
-const reviewForm = document.querySelector("#review-form");
-console.log(reviewForm);
+//   handleReview(e.target.new_review.value);
+//   reviewForm.reset();
+// });
 
-reviewForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  handleReview(e.target.new_review.value);
-  reviewForm.reset();
-});
 
+
+function openTheForm() {
+  document.getElementById("popupForm").style.display = "block";
+  grabForm();
+}
+
+function grabForm() {
+  const reviewForm = document.querySelector(".formContainer");
+  console.log(".formContainer");
+  reviewForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleReview(e.target.new_review.value);
+    reviewForm.reset();
+    closeTheForm()
+  });
+
+  const cancelBtn = document.querySelector(".cancel")
+  cancelBtn.addEventListener("click", closeTheForm)
+
+}
+  
+ 
 function handleReview(review) {
   const reviewLeft = document.createElement("p");
+  reviewLeft.id = "review"
   reviewLeft.textContent = review;
-  document.querySelector("#review-container").appendChild(reviewLeft);
+  document.querySelector(".movie-card").appendChild(reviewLeft);
 }
+
+function closeTheForm() {
+  document.getElementById("popupForm").style.display = "none";
+}
+
